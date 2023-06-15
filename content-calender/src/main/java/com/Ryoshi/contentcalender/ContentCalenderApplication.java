03f671139917921f52ac737e5b74de67049fd653
Ryoshi <fischerklon3@gmail.com>
2023-06-15 15:10:51 +0200

    First Files

---
+++ content-calender/src/main/java/com/Ryoshi/contentcalender/ContentCalenderApplication.java
@@ -0,0 +1,13 @@
+package com.Ryoshi.contentcalender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCalenderApplication.class, args);
+	}
+
+}

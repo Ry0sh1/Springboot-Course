a1557a8ec4acc576c4ba86247484b5a310c5e28a
Ryoshi <fischerklon3@gmail.com>
2023-06-16 14:12:58 +0200

    1:44:31 https://www.youtube.com/watch?v=UgX5lgv4uVM Spring Boot Tutorial for Beginners - 2023 Crash Course using Spring Boot 3

--- src/main/java/com/Ryoshi/contentcalender/controller/ContentController.java
+++ src/main/java/com/Ryoshi/contentcalender/controller/ContentController.java
@@ -2,6 +2,7 @@ package com.Ryoshi.contentcalender.controller;
 
 import com.Ryoshi.contentcalender.model.Content;
 import com.Ryoshi.contentcalender.repository.ContentCollectionRepository;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin
 public class ContentController {
 
     private final ContentCollectionRepository repository;
@@ -31,13 +33,13 @@ public class ContentController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public void create(@RequestBody Content content){
+    public void create(@Valid @RequestBody Content content){
         repository.save(content);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    public void update(@RequestBody Content content, @PathVariable Integer id){
+    public void update(@Valid @RequestBody Content content, @PathVariable Integer id){
         if (!repository.existsById(id)){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
--- src/main/java/com/Ryoshi/contentcalender/model/Content.java
+++ src/main/java/com/Ryoshi/contentcalender/model/Content.java
@@ -1,9 +1,13 @@
 package com.Ryoshi.contentcalender.model;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
 public record Content(
+
         Integer id,
+        @NotBlank
         String title,
         String desc,
         Status status,
